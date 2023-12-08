114f355fda10c5f7eb4879d4b754b9cc4395beb6
Asem hagi hasan <asemhagihasan12345@gmail.com>
2023-12-08 22:33:50 +0300

    Created the database and controlars

---
+++ Proje-B201210567/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proje_B201210567.Models
+{
+	public class Admin
+	{
+		[Key]
+        public int Id { get; set; }
+		public string Adi { get; set; }
+        public int Soyad { get; set; }
+		public string Email {  get; set; }
+        public string Sifre { get; set; }
+    }
+}
--- Proje-B201210567/Models/Kullanci.cs
+++ Proje-B201210567/Models/Kullanci.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Proje_B201210567.Models
 {
@@ -6,15 +7,29 @@ namespace Proje_B201210567.Models
 	{
         [Key]
         public int KullaniciId { get; set; }
-        [Required]
+
+		[Required(ErrorMessage = "Kullanıcı Adı boş bırakılamaz.")]
+		[DisplayName("Adı")]
         public string Kullanci_Adi { get; set; }
-		[Required]
-		public string Kullanci_Soyad { get; set; }
+
+		[Required(ErrorMessage = "Kullanıcı Soyadı boş bırakılamaz.")]
+		[DisplayName("Soyad")]
+        public string Kullanci_Soyad { get; set; }
+
+		[Required(ErrorMessage = "Telefon Numarası boş bırakılamaz.")]
+		[DisplayName("Telefon Numarası")]
         public int TelefonNumarasi { get; set; }
-        public string Cinsel { get; set; }
-        [Required]
+
+		[Required(ErrorMessage = "Cinsel seçimi yapmalısınız.")]
+		public string Cinsel { get; set; }
+
+		[Required(ErrorMessage = "Email boş bırakılamaz.")]
+		[EmailAddress(ErrorMessage = "Geçerli bir email adresi giriniz.")]
+		[DisplayName("Email address")]
 		public string Email { get; set; }
-		[Required]
-		public string Sifre { get; set; }
+
+		[Required(ErrorMessage = "Şifre boş bırakılamaz.")]
+		[DisplayName("Sifre")]
+        public string Sifre { get; set; }
     }
 }
