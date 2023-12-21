b2efcd5d91e311fc8b05da35d5fd074b6779e4b8
Asem hagi hasan <asemhagihasan12345@gmail.com>
2023-12-21 13:03:13 +0300

    .

---
+++ Proje-B201210567/Models/RandevuModelleri.cs
@@ -0,0 +1,12 @@
+﻿namespace Proje_B201210567.Models
+{
+    public class RandevuModelleri
+    {
+        public List <Poliklinik> poliklinikler { get; set; }
+
+        public List<Doktor> doktorlar { get; set; }
+
+        public Kullanci Kullanci { get; set; }
+        
+    }
+}
