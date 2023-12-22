4dd3115f497af663191408f73df63556900c9a57
Asem hagi hasan <asemhagihasan12345@gmail.com>
2023-12-22 19:37:42 +0300

    updating Randevu controlaar and adding CalismaFiltere and creating two action

---
+++ Proje-B201210567/Models/CalismaSaatlarGroup.cs
@@ -0,0 +1,13 @@
+﻿namespace Proje_B201210567.Models
+{
+	public class CalismaSaatlarGroup
+	{
+        public int DoctorId { get; set; }
+        public string DoctorName { get; set; }
+        public string DoktorSoyad { get; set; }
+        public string BolumAdi { get; set; } = "";
+        public List<CalismaSaati> CalismaSaatiListesi { get; set; } = new List<CalismaSaati>() { };
+        public DateTime BaslangisTarih { get; set; }
+        public DateTime BitisTarih { get; set; }
+    }
+}
