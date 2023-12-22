9e4d94b3c4ae85e534d21c13cff0e9c8a814380c
Muhammed <muhammedkerek2000@gmail.com>
2023-12-22 13:47:21 +0300

    .

--- Proje-B201210567/Models/Poliklinik.cs
+++ Proje-B201210567/Models/Poliklinik.cs
@@ -14,5 +14,6 @@ namespace Proje_B201210567.Models
         [ForeignKey("Doktor")]
         public int ?DoktorId { get; set; }
         public List<Doktor> DoktorList { get; set;}
+        
     }
 }
