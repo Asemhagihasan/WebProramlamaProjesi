ee510b40f185b095f48efd9b65c0171cc932e797
Muhammed <muhammedkerek2000@gmail.com>
2023-12-26 21:34:47 +0300

    Added The Admin Controller And It's Api

--- Proje-B201210567/Models/Admin.cs
+++ Proje-B201210567/Models/Admin.cs
@@ -8,7 +8,7 @@ namespace Proje_B201210567.Models
         public int Id { get; set; }
         public string Tc { get; set; }
         public string Adi { get; set; }
-        public int Soyad { get; set; }
+        public string Soyad { get; set; }
         public string TelefonNumarasi { get; set; }
         public string Email {  get; set; }
         public string Sifre { get; set; }
